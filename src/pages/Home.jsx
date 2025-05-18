92e1e22dba478a02d41fe1e5a2b63d9e322cf5bb
dlehd <dlehdrb5509@naver.com>
2025-05-18 14:14:42 +0900

    Feat : Home 페이지 그라데이션 처리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,7 @@ import HahmletHeading from "../components/HahmletHeading";
 import SDGothicBody from "../components/SDGothicBody";
 
 const Home = () => {
-
-
-
-
   const imageSources = [
-
-    // 더 많은 이미지 URL 추가
     require('../assets/SampleImages/Image1.jpg'),
     require('../assets/SampleImages/Image2.jpg'),
     require('../assets/SampleImages/Image3.jpg'),
@@ -21,29 +15,43 @@ const Home = () => {
     require('../assets/SampleImages/Image6.jpg'),
     require('../assets/SampleImages/Image7.jpg'),
     require('../assets/SampleImages/Image8.jpg'),
-    
   ];
 
   return (
-    <div >
-      <Grid className={'px-32 pt-20'}>
-        <Col className={'flex-1'}>
+    <div className="relative overflow-hidden">
+      {/* 왼쪽 그라데이션 (넓고 자연스럽게) */}
+      <div className="absolute left-0 top-0 h-full w-[65%] z-10 pointer-events-none bg-gradient-to-r from-black/40 to-transparent" />
+
+
+      {/* 오른쪽 그라데이션 */}
+      <div className="absolute right-0 top-0 h-full w-[25%] z-10 pointer-events-none bg-gradient-to-l from-black/40 to-transparent" />
+
+      {/* 위쪽 블러 그라데이션 */}
+      <div className="absolute top-0 left-0 w-full h-10 bg-gradient-to-b from-white to-transparent" />
+  
+      {/* 아래쪽 블러 그라데이션
+      <div className="absolute bottom-0 left-0 w-full h-10 bg-gradient-to-t from-white to-transparent" /> */}
+
+      <Grid className="px-32 pt-20 relative z-0">
+        <Col className="flex-1">
           <Row>
-              <HahmletHeading className={'mb-8'}>
-                  당신의 추억을 다시 듣고 싶으신가요?
-              </HahmletHeading>
+            <HahmletHeading className="mb-8">
+              당신의 추억을 다시 듣고 싶으신가요?
+            </HahmletHeading>
           </Row>
           <Row>
-              <SDGothicBody className={'mb-5'}>
-                  잊어버린 당신의 사진첩 속 기억 한 조각을 가져오세요<br />
-                  눈을 감고 시간을 되감아봅시다.<br />
-              </SDGothicBody>
+            <SDGothicBody className="mb-5">
+              잊어버린 당신의 사진첩 속 기억 한 조각을 가져오세요<br />
+              눈을 감고 시간을 되감아봅시다.<br />
+            </SDGothicBody>
           </Row>
         </Col>
       </Grid>
-      <FrameSlideshow images={imageSources}  className={'mt-32'}/>
-    </div>
 
+      <div className="mt-32 relative z-0">
+        <FrameSlideshow images={imageSources} />
+      </div>
+    </div>
   );
 };
 
